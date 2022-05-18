b2e1c57786931544fcff31a0b072415951c47da8
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2022-05-18 23:35:25 +0700

    config menu header

---
+++ src/app/clothes-shop/constance/menu-header-path.ts
@@ -0,0 +1,34 @@
+export const headerPath = {
+  menu: [
+    {
+      name: 'HOME',
+      url: '/welcome/intro',
+    },
+    {
+      name: 'SHOP',
+      url: '/shop-product'
+    },
+    {
+      name: 'CART',
+      url: '/cart'
+    },
+    {
+      name: 'CONTACT',
+      url: '/contact-us'
+    }
+  ],
+  iconContact: [
+    {
+      icon: 'bi bi-facebook',
+      url: 'https://www.facebook.com/nguyen.lam.19.01'
+    },
+    {
+      icon: 'bi bi-twitter',
+      url: 'https://twitter.com/?lang=vi'
+    },
+    {
+      icon: 'bi bi-instagram',
+      url: 'https://www.instagram.com/'
+    }
+  ]
+};
--- src/app/pages/_layout/components/topbar/topbar.component.ts
+++ src/app/pages/_layout/components/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {headerPath} from '../../../../clothes-shop/constance/menu-header-path';
 
 @Component({
   selector: 'app-topbar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
+  listMenu = headerPath.menu;
+  listIconContact = headerPath.iconContact;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getActiveMenu(url: string): string{
+    const urlNow = window.location.href;
+    if (urlNow.indexOf(url) !== -1){
+      return 'active-item';
+    }else {
+      return  '';
+    }
+  }
 }
